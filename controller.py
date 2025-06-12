ac17a5dadeaef3a99d8babae5845f2ea28df5a23
t41g4 <c4p31048@bunkyo.ac.jp>
2025-06-12 14:59:50 +0900

    syousai.htmlにつながるようpythonファイルを変更し、データベースのデータをhtmlに表示させれるようにした.item_listは実験用

--- controller.py
+++ controller.py
@@ -46,7 +46,7 @@ def j_user():
 
 def j_item(key=""):
     lastdt = db.session.query(拾得物管理状況.拾得物ID.label("oid"),func.max(拾得物管理状況.変更日時).label("last")).group_by(拾得物管理状況.拾得物ID).subquery()
-    tmp = db.session.query(拾得物管理状況,拾得物,拾得物分類,ユーザ,所属).filter(拾得物管理状況.拾得物ID==拾得物.ID).filter(拾得物.拾得物分類ID==拾得物分類.ID).filter(拾得物管理状況.ユーザID==ユーザ.ID).filter(ユーザ.所属ID==所属.ID).filter(拾得物管理状況.変更日時==lastdt.c.last).filter(拾得物管理状況.拾得物ID==lastdt.c.oid)
+    tmp = db.session.query(拾得物管理状況,拾得物,拾得物分類,ユーザ,所属).filter(拾得物管理状況.拾得物ID==拾得物.ID).filter(拾得物.拾得物分類ID==拾得物分類.ID).filter(拾得物管理状況.管理者ID==ユーザ.ID).filter(ユーザ.所属ID==所属.ID).filter(拾得物管理状況.変更日時==lastdt.c.last).filter(拾得物管理状況.拾得物ID==lastdt.c.oid)
     if key == "":
         data = tmp.all()
     else:
@@ -91,3 +91,31 @@ def j_request():
             "特徴": i.特徴
         })
     return obj
+
+def d_item(id):
+    data = db.session.query(拾得物管理状況,ユーザ).filter(拾得物管理状況.管理者ID==ユーザ.ID).filter(拾得物管理状況.拾得物ID==id)
+    list = []
+    for i,j in data:
+        list.append({
+            "変更日時":i.変更日時,
+            "変更内容":i.変更内容,
+            "氏名":j.氏名,
+        })
+    i,j = db.session.query(拾得物,拾得物分類).filter(拾得物.拾得物分類ID==拾得物分類.ID).filter(拾得物.ID==id).first()
+    if i.画像 is None:
+        i.画像 = ""
+    d = {
+        "大分類":j.大分類,
+        "物品名":j.物品名,
+        "拾得場所":i.拾得場所,
+        "色":i.色,
+        "特徴":i.特徴,
+        "画像":i.画像, 
+        "変更履歴":list, 
+        "id":id
+        }
+    return render_template("syousai.html", d=d, u=j_user())
+
+def f_item(k):
+    j = j_item(k)
+    return render_template("item_list.html", j=j, n=len(j))
--- route.py
+++ route.py
@@ -15,36 +15,46 @@ def create_route(app):
     def top():
         return render_template("top.html")
 
-    @app.route("/item_list")
+    @app.route("/item_list", methods=["GET","POST"])
     def item_list():
-        return render_template("item_list.html")
+        k = request.form.get("key") if "key" in request.form else ""
+        return controller.f_item(k)
 
-    @app.route("/item_detail")
+    @app.route("/item_detail", methods=["GET","POST"])
     def item_detail():
-        return render_template("syousai.html")
+        if request.method == "POST":
+            id = request.form.get("id")
+            d = {
+                "拾得物ID":id, 
+                "変更内容":request.form.get("act"), 
+                "管理者ID":request.form.get("user"), 
+            }
+            controller.act_reg(d)
+            return redirect(f"/item_detail?id={id}")
+        if "id" in request.args:
+            id = request.args.get("id")
+            return controller.d_item(id)
+        return("GETパラメータを指定してください")
 
     @app.route("/reg_user")
     def reg_user():
         return render_template("reg_user.html")
 
-    @app.route("/reg_item")
-    def reg_item():
-        return render_template("reg_item.html")
-
-    @app.route("/reg_item", methods=["GET","POST"])
+    @app.route("/reg_item", methods=["GET", "POST"])
     def reg_item():
         if request.method == "POST":
             file = request.files["img"]
-            d= {
-                "拾得物分類ID":request.form.get("category"),
-                "拾得場所":request.form.get("place"),
-                "色":request.form.get("color"),
-                "特徴":request.form.get("spec"),
-                "ユーザID":request.form.get("user"),
-            }
+            d = {
+                "拾得物分類ID": request.form.get("category"),
+                "拾得場所": request.form.get("place"),
+                "色": request.form.get("color"),
+                "特徴": request.form.get("spec"),
+                "ユーザID": request.form.get("user"),
+                }
             controller.item_reg(d, file)
             return redirect("/reg_item")
-        return controller.item_form()
+        # GETリクエスト時はフォーム表示
+        return render_template("reg_item.html")
 
     @app.route("/req_list")
     def req_list():
